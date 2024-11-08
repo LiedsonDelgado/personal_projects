d5e7a32910cc9ccf04631949ab7cf4c0286d00f5
liedson__delgado <liedsondelgado38@gmail.com>
2024-11-08 22:32:52 -0100

    Add files via upload

---
+++ C/ATM BANK/ATM SYSTEM PROJECT/ATM System Project v.1.0.0/main.c
@@ -0,0 +1,13 @@
+#include "AtmBank.h"
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h>
+#include <time.h>
+
+
+
+int main(){
+    setColor(2); //(2=cor verde)
+setlocale(LC_ALL,"PORTUGUESE");//Caracteres do Portugues(ex.:acentos,etc)
+
+}
