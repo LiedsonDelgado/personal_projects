bf366c674f2420ecfd1da833d9f66bc014a857df
Liedson Delgado <liedsondelgado38@gmail.com>
2025-05-27 18:49:18 -0100

    Update window.java

--- Java/JanelaJava/window.java
+++ Java/JanelaJava/window.java
@@ -7,17 +7,17 @@ public class window{
 
         for(i = 0;i < 4;i++){
 
-    try{
-        Thread.sleep(500); //Tread.sleep(####...) <== Miliseconds
-            JFrame window = new JFrame();
+            try{
+                Thread.sleep(500); //Tread.sleep(####...) <== Miliseconds
+                JFrame window = new JFrame();
                 window.setSize(500,500); //(width,height);
                 window.getContentPane().setBackground(new Color(0,200,0)); //new Color(#RED,#GREE,#BLUE) <== RGB
                 window.setVisible(true);
                 window.setTitle("JAVA WINDOW");
-
-            }catch(InterruptedException e){
-                System.out.println("End of the Sleep!");
-            }    
+        
+                }catch(InterruptedException e){
+                    System.out.println("End of the Sleep!");
+                }    
         }
     }
 }
